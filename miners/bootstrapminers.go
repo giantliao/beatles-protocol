bae288a7f78c4048f55312fa0143cb7341e86e73
giantliao <giantliao@tutanota.com>
2020-08-15 15:48:52 +0800

    add to repo

--- miners/bootstrapminers.go
+++ miners/bootstrapminers.go
@@ -7,17 +7,30 @@ import (
 	"github.com/kprc/libeth/util"
 )
 
+type GithubDownLoadPoint struct {
+	Owner      string `json:"ownew"`
+	Repository string `json:"repository"`
+	Path       string `json:"path"`
+	ReadToken  string `json:"access_token"`
+}
+
 type BootsTrapMiners struct {
-	Boots             []*Miner              `json:"boots"`
-	BeatlesMasterAddr account.BeatleAddress `json:"beatles_master_addr"`
-	EthAccPoint       string                `json:"eth_acc_point"`
-	TrxAccPoint       string                `json:"trx_acc_point"`
-	NextDownloadPoint []string              `json:"next_download_point"`
+	Boots             []*Miner               `json:"boots"`
+	BeatlesMasterAddr account.BeatleAddress  `json:"beatles_master_addr"`
+	BeatlesEthAddr    string                 `json:"beatles_eth_addr"`
+	BeatlesTrxAddr    string                 `json:"beatles_trx_addr"`
+	EthAccPoint       string                 `json:"eth_acc_point"`
+	TrxAccPoint       string                 `json:"trx_acc_point"`
+	NextDownloadPoint []*GithubDownLoadPoint `json:"next_download_point"`
 }
 
 var seckey = `We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, 
 				provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, 
 				do ordain and establish this Constitution for the United States of America.`
+
+func (gd *GithubDownLoadPoint) String() string {
+	return "owner: " + gd.Owner + "  repo: " + gd.Repository + "  path: " + gd.Path + "  token: " + gd.ReadToken
+}
 
 func (btms *BootsTrapMiners) Marshal(key []byte) ([]byte, error) {
 	j, _ := json.Marshal(*btms)
